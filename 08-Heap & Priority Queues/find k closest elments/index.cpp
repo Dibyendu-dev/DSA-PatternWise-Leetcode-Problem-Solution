57c425917bf92a8a3f3e0e45d74982dc49f86492
ddas4548 <ddas4548@gmail.com>
2025-11-25 10:30:38 +0530

    implement find k closest  element

---
+++ 08-Heap & Priority Queues/find k closest elments/index.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int,int>>pq;
+
+        for(int i=0; i<arr.size();i++){
+            pq.push({abs(arr[i]-x),arr[i]});
+
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+        vector<int> result;
+        while(pq.size()>0){
+            result.push_back(pq.top().second) ;
+            pq.pop();
+        }
+
+        sort(result.begin(),result.end());
+        return result;
+    }
+};
