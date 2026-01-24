ceae21f75f4da17d9b45908907d7ae094bf4ab2b
Murtuza27 <Syedmurtuzamuteeb35@gmail.com>
2026-01-24 10:55:04 +0530

    implement undirected graph using adjacency matrix

---
+++ 10-Graph/01-Implementation/01-undirected graph/1-implement with adjacency matrix/index.py
@@ -0,0 +1,25 @@
+def createGraph(V,edges):
+    mat =[[0 for _ in range (V)] for _ in range (V)]
+
+    for it in edges:
+        u = it[0]
+        v = it[1]
+        mat[u][v] = 1
+        mat[v][u] = 1
+    
+    return mat
+
+if __name__ == "__main__":
+    v=3
+    edges=[[0,1],[1,0],[1,2]]
+
+    mat = createGraph(v,edges)
+
+    print("adjacency matrix representattion of undirected graph")
+    for i in range(v):
+        for j in range (v):
+            print(mat[i][j],end=" ")
+        print()
+            
+
+
