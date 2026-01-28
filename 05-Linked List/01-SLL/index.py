8bba08bc9af4ce7ca57ac93757fd3dc02cc6379c
Murtuza27 <ddas4548@gmail.com>
2026-01-28 14:17:54 +0530

    implemnt inserAtLast and insertAtAny position fn

--- 05-Linked List/01-SLL/index.py
+++ 05-Linked List/01-SLL/index.py
@@ -16,6 +16,38 @@ def inserAtFirst(head,data):
     newnode.next=head
     return newnode
 
+def insertAtLast(head,data):
+    newnode = Node(data)
+    if head is None:
+        return newnode
+    
+    curr = head
+    while curr.next is not None:
+        curr= curr.next
+
+    curr.next = newnode
+    return head
+
+def insertAtAny(head,data,pos):
+    newnode = Node(data)
+    
+    if pos < 1:
+        return head
+    
+    if pos == 1:
+        newnode.next=head
+        return newnode
+    
+    curr = head
+    for i in range(0,pos-1):
+        if curr is None:
+            return head
+        curr=curr.next
+    newnode.next = curr.next
+    curr.next = newnode
+    return head
+
+
 def printSLL(head):
     curr = head
     while curr:
@@ -35,3 +67,9 @@ if __name__ == "__main__":
     printSLL(head)
     head = inserAtFirst(head,55)
     printSLL(head)
+    head = insertAtLast(head,333)
+    printSLL(head)
+    head = insertAtAny(head,4548,2)
+    printSLL(head)
+
+
