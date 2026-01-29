f680d9f77c8957af23d221032a5aa8a807f83238
Murtuza27 <ddas4548@gmail.com>
2026-01-29 17:16:06 +0530

    implement reorder routes

---
+++ 10-Graph/05-reorder routes/index.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        graph = [[] for _ in range(n)]
+
+        for a, b in connections:
+            graph[a].append((b,1))
+            graph[b].append((a,0))
+
+        visited = [False] * n
+
+        def dfs(node):
+            visited[node] = True
+            changes = 0
+
+            for neighbor,cost in graph[node]:
+                if not visited[neighbor]:
+                    changes += cost
+                    changes += dfs(neighbor)
+                
+            return changes
+
+        return dfs(0)
