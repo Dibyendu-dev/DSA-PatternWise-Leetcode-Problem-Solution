3604358d1c49630c246a480f6a31ca03cd151ae3
Murtuza27 <ddas4548@gmail.com>
2026-04-04 17:27:00 +0530

    implement rotate a ll by k

---
+++ 05-Linked List/13-Rotate a LL/index.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or not head.next or k == 0:
+            return head
+
+        temp = head
+        length =1
+
+        while temp.next:
+            length += 1
+            temp = temp.next
+
+        temp.next = head
+        k = k % length
+
+        end = length - k
+        while end > 0:
+            temp = temp.next
+            end -=1
+
+        head = temp.next
+        temp.next = None
+
+        return head 
